0c7c4a04de43816076e86c92e8c7e12ca79714f1
EricScottRichards <ericscottrichards@gmail.com>
2015-05-21 20:45:34 -0600

    Finished step 5

---
+++ js/userService.js
@@ -0,0 +1,14 @@
+var app = angular.module('rtfmApp');
+app.service('userService', function(fb){
+
+	var user = {};
+
+	this.login = function(username){
+		console.log(username);
+	}
+
+	this.getUser = function(){
+		return user;
+	}
+
+})
